edbe181248b355f6dea43d4e3d32c8ee725a8609
Farid Andika <faridandika159@gmail.com>
2024-05-08 16:05:56 +0700

    Initial Commit

---
+++ ImageApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageApplication
+//
+//  Created by Farid Andika on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageApplication/ImageApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageApplicationApp.swift
+//  ImageApplication
+//
+//  Created by Farid Andika on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
