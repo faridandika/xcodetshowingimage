60182d23463871b72f0cdbdfdffa68ede0102091
Farid Andika <faridandika159@gmail.com>
2024-05-20 20:56:46 +0700

    nothing changes

--- ImageApplication/ContentView.swift
+++ ImageApplication/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 print("Asiaapp")
             }
             
+            
             Image("flower")
                 .resizable()
                 .imageScale(.small)
